deda61ab30200f4ff618365e9c01f203028345d6
NandniGupta <nandanigupta42134@gmail.com>
2026-05-26 23:54:42 +0530

    add new backend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import {app} from './src/app.js';
+import dotenv from "dotenv";
+import connectDB from "./src/db/index.js";
+
+dotenv.config({
+    path:"./.env"
+});
+
+const PORT=process.env.PORT || 8001;
+connectDB().then(()=>{
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})}).catch((err)=>{
+    console.error("Failed to connect to the database:", err);
+    process.exit(1);
+});
---
+++ backend/src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
+const connectDB=async()=>{
+    try{
+        const connection=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`MongoDB connected: ${connection.connection.host}`);
+    }catch(err){
+            console.error("Error connecting to MongoDB:", err);
+            process.exit(1);
+    }
+}
+export default connectDB;
